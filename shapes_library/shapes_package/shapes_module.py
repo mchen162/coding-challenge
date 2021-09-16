859e8d48e53862223d4d746e23a8f4c303d40e79
Melissa Chen <melissacxj2016@gmail.com>
2021-09-16 10:19:08 -0700

    Created tests for Circle, Rectangle, and RightTriangle

--- shapes_library/shapes_package/shapes_module.py
+++ shapes_library/shapes_package/shapes_module.py
@@ -6,10 +6,14 @@ class Circle:
     This class implements the Circle API
     """
     def __init__(self, radius: float = None):
-        pass
-
+        #constructor/object--> stuff in the ()
+        if radius < 0:
+            raise ValueError("Supplied radius must be greater than 0")
+            #raises is the same as "throw" in java --> used to throw an exception from a method or any block of code
+        self.radius=radius
+        
     def area(self):
-        pass
+        return round(math.pi*self.radius**2, 2)
 
 
 class Rectangle:
@@ -17,10 +21,37 @@ class Rectangle:
     This class implements the Rectangle API
     """
     def __init__(self, height: float = None, width: float = None):
-        pass
+        if height < 0 or width < 0:
+            raise ValueError("Supplied width or height must be greater than 0")
+        
+        self.height=height
+        self.width=width
         
     def area(self):
-        pass
+        return round(self.height*self.width, 2)
 
 
+class RightTriangle:
+    """
+    This class implements the Right Triangle API
+    """
+    def __init__(self, height: float = None, width: float = None, hypotenuse: float = None):
+        if height < 0 or width < 0 or hypotenuse < 0:
+            raise ValueError("Supplied width, height, or hypotenuse must be greater than 0")
+        
+        self.height=height
+        self.width=width
+        self.hypotenuse=hypotenuse
+
+    def pythagorean(self):
+        theorem = self.height**2 + self.width**2 
+        if theorem == self.hypotenuse**2:
+            return True
+        else:
+            return False
+            
+    def area(self):
+        return round(0.5*self.height*self.width, 2)
+
+    
         
--- tests/shapes_package/test_shapes.py
+++ tests/shapes_package/test_shapes.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from shapes_library.shapes_package.shapes_module import Circle
 from shapes_library.shapes_package.shapes_module import Rectangle
+from shapes_library.shapes_package.shapes_module import RightTriangle
 
 
 class CircleTests(TestCase):
@@ -8,6 +9,7 @@ class CircleTests(TestCase):
     Contains all unit tests for the shapes_module.Circle class
     """
     def test_circle_area_returns_correct_result_with_valid_radius(self):
+#common to have long descriptive names for functions
         """
         Tests that a Circle object with a radius > 0 returns the correct area
         """
@@ -15,7 +17,17 @@ class CircleTests(TestCase):
         expected_area = 78.54
         mock_circle = Circle(radius=mock_radius)
         self.assertEqual(mock_circle.area(), expected_area)
+#assert equal is a built in method in unittest library, 1 of 15 built in to compare 2 things, b/c it's a test
+#mock_circle.area() & expected_area --> 2 parameters being compared by the assertEqual method
+#self --> function
+#self is a keyword: means "this" sorta like itself?????????
 
+    def test_circle_area_raises_value_error_with_negative_radius(self):
+        """
+        Tests that Circle object raises value error with negative radius
+        """
+        with self.assertRaises(ValueError):
+            mock_circle = Circle(radius=-1.0)
 
 class RectangleTests(TestCase):
     """
@@ -30,4 +42,45 @@ class RectangleTests(TestCase):
         mock_rectangle = Rectangle(height=mock_height, width=mock_width)
         self.assertEqual(mock_rectangle.area(), expected_area)
 
+    def test_rectangle_area_raises_value_error_with_negative_width_or_height(self):
+        """
+        Tests that Rectangle object raises value error with negative width or height
+        """
+        with self.assertRaises(ValueError):
+            mock_rectangle = Rectangle(height=-1.0, width=-1.0)
+
+
+class RightTriangleTests(TestCase):
+    """
+    Contains all the unit tests for the shapes_module.RightTriangle class
+    """
+    def test_right_triangle_returns_correct_result_with_valid_height_and_width(self):
+        """
+        Tests that a Right Triangle object with a height > 0, width > 0, and a hypotenuse > 0 returns the correct area
+        """
+        mock_height, mock_width, mock_hypotenuse = 3., 4., 5.
+        expected_area = 6.
+        mock_right_triangle = RightTriangle(height=mock_height, width=mock_width, hypotenuse=mock_hypotenuse)
+        self.assertEqual(mock_right_triangle.area(), expected_area)
+  
+    def test_right_triangle_raises_error_message_with_valid_height_and_width_and_hypotenuse(self):
+        """
+        Tests that Right Triangle object raises value error with width, height, and hypotenuse that are consistent with the Pythagorean Theorem
+        """
+        mock_height, mock_width, mock_hypotenuse = 3., 4., 5.
+
+        mock_right_triangle = RightTriangle(height=mock_height, width=mock_width, hypotenuse=mock_hypotenuse)
+        self.assertIs(mock_right_triangle.pythagorean(), True, "Test value is not true")
+    
+    def test_right_triangle_raises_value_error_with_negative_width_or_height_or_hypotenuse(self):
+        """
+        Tests that Right Triangle object raises value error with negative width, height, or hypotenuse
+        """
+        with self.assertRaises(ValueError):
+            mock_right_triangle = RightTriangle(height=-1.0, width=-1.0, hypotenuse=-1.0)
+
+    #def test_right_triangle_raises_value_error_with_valid_hypotenuse(self):
+        """
+        Tests that Right Triangle object raises value error with greater width or height in relation to hypotenuse
+        """
 
